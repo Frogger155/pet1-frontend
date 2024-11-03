eca1669de1b3050484bdb96c4059320cb7914e8a
Frogger155 <nikandrkudryavtsev@gmail.com>
2024-11-03 21:50:20 +1000

    исправил ошибку в компоненте, подружающем посты

--- src/MainContent/Components/CardList.jsx
+++ src/MainContent/Components/CardList.jsx
@@ -32,7 +32,7 @@ export default function CardList() {
 
     return (
         <>
-            {
+            { posts && posts.map &&
                 posts.map((post) => (
                     <>
                         <Card
